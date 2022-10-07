a51a6e800e2e677d69796c04c99c0a1f953e5a33
DnastyL <gomes.lucas8000@gmail.com>
2022-10-07 19:19:58 -0300

    triyngtofix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ApolloProvider } from "@apollo/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./context/Context";
 import { client } from "./lib/apollo";
 import { Router } from "./Router";
@@ -7,11 +7,11 @@ import { Router } from "./Router";
 function App() {
   return (
     <ApolloProvider client={client}>
-      <HashRouter>
         <ContextProvider>
-          <Router />
+          <BrowserRouter>       
+            <Router />
+          </BrowserRouter>
         </ContextProvider>
-      </HashRouter>
     </ApolloProvider>
   );
 }
