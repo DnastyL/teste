1b73be6dbcdf8b94150cf2fbba18d4476dd10299
DnastyL <gomes.lucas8000@gmail.com>
2022-10-07 17:50:13 -0300

    upOnRoutes

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,25 +5,33 @@ import { Subscribe } from "./pages/Subscribe";
 import { SubscribeTeacher } from "./pages/SubscribeTeacher";
 
 export const Router = () => {
-  const { sessionStorageTeacher, sessionStorageSubscriber } = useContextValues();
+  const { sessionStorageTeacher, sessionStorageSubscriber } =
+    useContextValues();
 
   return (
     <Routes>
-      <>
-        <Route path="/" element={<Subscribe />} />
-        <Route path="/instructor" element={<SubscribeTeacher />} />
-        <Route path="/login" element={<Subscribe />} />
-        <Route path="/instructor/login" element={<SubscribeTeacher />} />
-        <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/" element={<Subscribe />} />
+      <Route path="/instructor" element={<SubscribeTeacher />} />
+      <Route path="/login" element={<Subscribe />} />
+      <Route path="/instructor/login" element={<SubscribeTeacher />} />
+      <Route path="*" element={<Navigate to="/" />} />
 
-        {sessionStorageSubscriber && (
-          <>
-            <Route path="/event/:subscriber" element={<Event />} />
-            <Route path="/event/:subscriber/lesson/:lessonSlug" element={<Event />}/>
-            <Route path="/event/*" element={<Navigate to={`/event/${sessionStorageSubscriber.slug}`} />} />
-          </>
-        )}
-      </>
+      {sessionStorageSubscriber && (
+        <>
+          <Route path="/event/:subscriber" element={<Event />} />
+          <Route
+            path="/event/:subscriber/lesson/:lessonSlug"
+            element={<Event />}
+          />
+          <Route
+            path="/event/*"
+            element={
+              <Navigate to={`/event/${sessionStorageSubscriber.slug}`} />
+            }
+          />
+        </>
+      )}
+
       {sessionStorageTeacher && (
         <>
           <Route path="instructor/event/:teacherSlug" element={<Event />} />
